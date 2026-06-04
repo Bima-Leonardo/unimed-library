042dfe55b8bd895186a1fe72675993c99e451c4f
Bima-Leonardo <bimaginting211@gmail.com>
2026-06-04 13:08:06 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,15 @@
-import streamlit as str
+import streamlit as st
+import mysql.connector
 
-def get_connection():
+def connect_db():
     # Mengambil data koneksi otomatis dari Streamlit Secrets Cloud
-    conn = str.connection("mysql")
+    secrets = st.secrets["connections"]["mysql"]
+    
+    conn = mysql.connector.connect(
+        host=secrets["host"],
+        port=secrets["port"],
+        database=secrets["database"],
+        user=secrets["username"],
+        password=secrets["password"]
+    )
     return conn
